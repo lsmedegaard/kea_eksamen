3f967c8a096ceb5117eeb09e97bf0d922cb4da9f
Lasse Smedegaard <Smedegaard@Lasses-MacBook-Pro.local>
2021-01-12 10:03:57 +0100

    stable

---
+++ toggle.js
@@ -0,0 +1,9 @@
+let menuOpened = false
+function toggle(baseUrl) {
+    menuOpened = !menuOpened
+    document.getElementById('toggle').src = menuOpened ? baseUrl + 'close.svg' : baseUrl + 'open.svg'
+    let main = document.getElementById('main');
+    let nav = document.getElementById('navigation');
+    main.classList.toggle('active');
+    nav.classList.toggle('active');
+}
